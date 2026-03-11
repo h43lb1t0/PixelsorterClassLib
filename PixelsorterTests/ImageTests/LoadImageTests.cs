8578eb27ab84cf916adc6f606474f1258c691354
Tom Haelbich <haelbito@outlook.com>
2026-03-11 17:17:51 +0100

    Add image format/channel tests and helper utilities
    Added ImageTestHelpers for generating test and corrupted images.
    Introduced LoadImageTests with parameterized tests to verify
    LoadImage supports multiple formats and channel counts. Test
    data covers all combinations of supported extensions and channels.

---
+++ PixelsorterTests/ImageTests/LoadImageTests.cs
@@ -0,0 +1,36 @@
+﻿using PixelsorterClassLib;
+
+namespace Pixelsorter.Tests.ImageTests
+{
+    public class LoadImageTests
+    {
+        [Theory]
+        [MemberData(nameof(GetImageFormatCombinations))]
+        public void LoadImage_ShouldSupportFormatAndChannels(string extension, int channels)
+        {
+            string path = ImageTestHelpers.CreateTestImage(extension, channels);
+            try
+            {
+                var image = PixelsorterClassLib.Image.LoadImage(path);
+
+                Assert.NotNull(image);
+            }
+            finally
+            {
+                if (File.Exists(path)) File.Delete(path);
+            }
+        }
+        public static TheoryData<string, int> GetImageFormatCombinations()
+        {
+            var data = new TheoryData<string, int>();
+            foreach (var ext in ImageTestHelpers.FileExtensions)
+            {
+                foreach (var chan in ImageTestHelpers.ChannelCounts)
+                {
+                    data.Add(ext, chan);
+                }
+            }
+            return data;
+        }
+    }
+}
